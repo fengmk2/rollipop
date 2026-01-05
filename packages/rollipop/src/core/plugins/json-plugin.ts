0f71696524b0fceb7f53c38ff34ca744c670a73c
leegeunhyeok <dev.ghlee@gmail.com>
2026-01-06 01:37:59 +0900

    feat: use `viteJsonPlugin` instead

--- packages/rollipop/src/core/plugins/json-plugin.ts
+++ packages/rollipop/src/core/plugins/json-plugin.ts
@@ -1,44 +1,21 @@
-import fs from 'node:fs';
-
 import { id, include } from '@rolldown/pluginutils';
 import type * as rolldown from 'rolldown';
+import { viteJsonPlugin } from 'rolldown/experimental';
 
 import { setFlag, TransformFlag } from './utils/transform-flags';
 
-function jsonPlugin(): rolldown.Plugin {
-  return {
-    name: 'rollipop:json',
-    load: {
+function jsonPlugin(): rolldown.Plugin[] {
+  const jsonMarker: rolldown.Plugin = {
+    name: 'rollipop:json-marker',
+    transform: {
       filter: [include(id(/\.json$/))],
       handler(id) {
-        const rawJson = fs.readFileSync(id, 'utf-8');
-        const json = JSON.parse(rawJson);
-
-        return {
-          code: jsonToEsm(json),
-          meta: setFlag(this, id, TransformFlag.SKIP_ALL),
-          moduleType: 'js',
-        };
+        return { meta: setFlag(this, id, TransformFlag.SKIP_ALL) };
       },
     },
   };
-}
 
-function jsonToEsm(data: Record<string, unknown>) {
-  const declarations: string[] = [];
-  const exports: string[] = [];
-  const exportDefaultMappings: string[] = [];
-
-  Object.entries(data).forEach(([key, value], index) => {
-    const identifier = `_${index}`;
-    declarations.push(`const ${identifier} = ${JSON.stringify(value)};`);
-    exports.push(`export { ${identifier} as "${key}" };`);
-    exportDefaultMappings.push(`"${key}":${identifier}`);
-  });
-
-  return [...declarations, ...exports, `export default {${exportDefaultMappings.join(',')}};`].join(
-    '\n',
-  );
+  return [jsonMarker, viteJsonPlugin({ namedExports: true })];
 }
 
 export { jsonPlugin as json };
